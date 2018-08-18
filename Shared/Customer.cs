c47f3c7c49aec8f3cafef5c40e3bb4122a54a9b5
Khuong Nguyen <khuongntrd@gmail.com>
2018-08-18 11:01:01 +0700

    Add project files.

---
+++ Shared/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    public class Customer
+    {
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Picture { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Address { get; set; }
+    }
+}
